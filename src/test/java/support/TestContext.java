f3aed5c5bd92dc954f52e51de966c5f269c8eb8c
uralJoy <marinaujoy@gmail.com>
2023-08-07 18:29:18 -0700

    linux false

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -32,7 +32,7 @@ public class TestContext {
     }
 
     public static void initialize() {
-        initialize("chrome", "local", true, true);
+        initialize("chrome", "local", true, false);
     }
 
     public static void teardown() {
